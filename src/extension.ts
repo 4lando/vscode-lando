699743f09c652722fdc1807b9514375a9bf321c8
Aaron Feledy <aaron@arrowonesolutions.com>
2023-09-21 22:38:22 -0500

    add basic lando command execution

--- src/extension.ts
+++ src/extension.ts
@@ -1,28 +1,50 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
+import { exec } from "child_process";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log(
-    'Congratulations, your extension "vscode-lando" is now active!'
-  );
+  const outputChannel = vscode.window.createOutputChannel("Lando");
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  const disposable = vscode.commands.registerCommand(
-    "extension.helloWorld",
-    () => {
-      // The code you place here will be executed every time your command is executed
+  let disposable = vscode.commands.registerCommand(
+    "extension.runLando",
+    async () => {
+      const command = await vscode.window.showInputBox({
+        prompt: "Enter Lando command",
+        placeHolder: "e.g., start, stop, rebuild",
+      });
 
-      // Display a message box to the user
-      vscode.window.showInformationMessage("Hello World!");
+      if (command) {
+        outputChannel.clear();
+        outputChannel.show(true);
+        outputChannel.appendLine(`Running: lando ${command}`);
+
+        exec(`lando ${command}`, (error, stdout, stderr) => {
+          if (error) {
+            outputChannel.appendLine(`Error: ${stderr}`);
+            vscode.window.showErrorMessage(
+              `Lando command failed. See output for details.`
+            );
+          } else {
+            outputChannel.appendLine(`Output: ${stdout}`);
+            vscode.window.showInformationMessage(
+              `Lando command executed successfully.`
+            );
+          }
+        });
+      }
+
+      if (command) {
+        exec(`lando ${command}`, (error, stdout, stderr) => {
+          if (error) {
+            vscode.window.showErrorMessage(`Error: ${stderr}`);
+          }
+        });
+      }
     }
   );
 
   context.subscriptions.push(disposable);
 }
+
+export function deactivate() {}
