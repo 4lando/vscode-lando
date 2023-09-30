834fde5d696368062a95d4e67ccd389817b08cc6
Aaron Feledy <aaron@arrowonesolutions.com>
2023-09-29 20:09:03 -0500

    use pty for stdin handling and colorful output

--- src/extension.ts
+++ src/extension.ts
@@ -1,50 +1,108 @@
 import * as vscode from "vscode";
-import { exec } from "child_process";
+import * as child_process from "child_process";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  const outputChannel = vscode.window.createOutputChannel("Lando");
-
-  let disposable = vscode.commands.registerCommand(
-    "extension.runLando",
-    async () => {
+  const writeEmitter = new vscode.EventEmitter<string>();
+  context.subscriptions.push(
+    vscode.commands.registerCommand("extension.runLando", async () => {
       const command = await vscode.window.showInputBox({
         prompt: "Enter Lando command",
         placeHolder: "e.g., start, stop, rebuild",
       });
 
-      if (command) {
-        outputChannel.clear();
-        outputChannel.show(true);
-        outputChannel.appendLine(`Running: lando ${command}`);
-
-        exec(`lando ${command}`, (error, stdout, stderr) => {
-          if (error) {
-            outputChannel.appendLine(`Error: ${stderr}`);
-            vscode.window.showErrorMessage(
-              `Lando command failed. See output for details.`
-            );
-          } else {
-            outputChannel.appendLine(`Output: ${stdout}`);
-            vscode.window.showInformationMessage(
-              `Lando command executed successfully.`
-            );
-          }
-        });
+      if (!command) {
+        return;
       }
 
-      if (command) {
-        exec(`lando ${command}`, (error, stdout, stderr) => {
-          if (error) {
-            vscode.window.showErrorMessage(`Error: ${stderr}`);
-          }
-        });
+      // TODO: Support multiple workspaces and multiple lando apps per workspace
+      let cwd = vscode.workspace.workspaceFolders?.[0].uri.fsPath;
+      if (!cwd) {
+        vscode.window.showErrorMessage("No workspace found");
+        return;
       }
-    }
+
+      // Start Lando process
+      const landoProcess = child_process.spawn("lando", command.split(" "), {
+        cwd: cwd,
+        shell: false,
+        env: { ...process.env, TERM: "xterm-256color", FORCE_COLOR: "true" },
+        killSignal: "SIGINT",
+      });
+
+      // Lando process output handling
+      landoProcess.stdout.on("data", (data) => {
+        // Terminal expects \r\n line endings
+        const output = data.toString().replace(/\n/g, "\r\n");
+        writeEmitter.fire(output);
+      });
+      landoProcess.stderr.on("data", (data) => {
+        // Terminal expects \r\n line endings
+        const output = data.toString().replace(/\n/g, "\r\n");
+        writeEmitter.fire(output);
+      });
+
+      // Terminal pty implementation
+      let line = "";
+      const pty = {
+        onDidWrite: writeEmitter.event,
+        open: () => writeEmitter.fire(`Running: lando ${command}\r\n\n`),
+        close: () => {
+          landoProcess.stdin.end();
+        },
+        handleInput: (data: string) => {
+          landoProcess.stdin.write(data);
+          if (data === "\x7f") {
+            // Backspace
+            if (line.length === 0) {
+              return;
+            }
+            line = line.substr(0, line.length - 1);
+            // Move cursor backward
+            writeEmitter.fire("\x1b[D");
+            // Delete character
+            writeEmitter.fire("\x1b[P");
+            return;
+          }
+          if (data === "\x03") {
+            // Ctrl+C
+            writeEmitter.fire("^C");
+            landoProcess.kill("SIGINT");
+            return;
+          }
+          if (data === "\x1b[A") {
+            // Up arrow
+            return;
+          }
+          if (data === "\x1b[B") {
+            // Down arrow
+            return;
+          }
+          if (data === "\x1b[C") {
+            // Right arrow
+            return;
+          }
+          writeEmitter.fire(data);
+        },
+      };
+      const terminal = vscode.window.createTerminal({
+        name: `Lando`,
+        pty,
+      });
+
+      terminal.show();
+
+      landoProcess.on("close", (code) => {
+        if (code !== 0) {
+          writeEmitter.fire(
+            `\x1b[31mLando process exited with code ${code}\x1b[0m\r\n`
+          );
+        }
+        // Disable terminal input
+        writeEmitter.fire("\x1b[?25l");
+        pty.handleInput = () => {};
+      });
+    })
   );
-
-  context.subscriptions.push(disposable);
 }
 
 export function deactivate() {}
